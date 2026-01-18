2ffac0edf6492deea205c65bda99053fd68a69d8
Djanfar Saindou <djsaindou@sio.sisr>
2026-01-18 13:26:18 +0300

    je vien de afficher les donne sur la la page offre en backoffice

--- BackOffice/offre.php
+++ BackOffice/offre.php
@@ -1,5 +1,21 @@
+<?php
+require_once(__DIR__ . '/bdd.php');
+
+
+$tabl=[];
+$fr=$mysqlClient->prepare('SELECT offres.Id, offres.Id_job, offres.Statut, offres.Ville, 
+job.Titre,job.Categorie, job.Description, job.Missions, job.Profil
+ FROM offres LEFT JOIN job ON job.Id = offres.Id_job;');
+$fr->execute();
+$tabl=$fr->fetchAll();
+
+
+
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +27,7 @@
         <div class="container header-inner">
             <a href="index.html" class="logo">
                 <span class="wave"></span>Lagon<span>Jobs</span>
+        </a>
             <nav class="nav">
                 <a href="index.php">Tableau de bord</a>
                 <a href="user.php">Utilisateurs</a>
@@ -24,73 +41,92 @@
     <section class="hero">
 
     <div class="container">
-        <form action="" method="GET" class="form">
+        <form action="" method="POST" class="form">
 
             <div>
                 <div class="row">
                     <label>
-                    Titre
-                    <input type="text" name="Sujet" required>
+                        Titre
+                            <select name="Titre" required>
+                                <?php
+                                    for ($i = 0; $i < count($tabl); $i++) {
+                                        echo '<option value="'.$tabl[$i]['Titre'].'">'. 
+                                        $tabl[$i]['Titre'].
+                                        '</option>';}
+                                ?>
+                            </select>
                     </label>
+
 
                     <label>
                         Ville
-                            <select name="type_contrat" required>
-                                <option value="CDI">Mamoudzou</option>
-                                <option value="CDD">CDD</option>
-                                <option value="Stage">Stage</option>
-                                <option value="Freelance">Freelance</option>
+                            <select name="ville" required>
+                                <?php
+                                    for ($i = 0; $i < count($tabl); $i++) {
+                                        echo '<option value="'.$tabl[$i]['Ville'].'">'. 
+                                        $tabl[$i]['Ville'].
+                                        '</option>';}
+                                ?>
                             </select>
                     </label>
                 </div>
 
                 <label>
-                    description
-                    <textarea name="description" required></textarea>
+                description
+                    <select name="description" required>
+                        <?php
+                            for ($i = 0; $i < count($tabl); $i++) {
+                                echo '<option value="'.$tabl[$i]['description'].'">'. 
+                                $tabl[$i]['description'].
+                                '</option>';}
+                        ?>
+                    </select>
                 </label>
             </div>
 
             <div class="row">
                 <label>
                 Statut
-                    <select name="type_contrat" required>
-                        <option value="CDI">publie</option>
-                        <option value="CDD">CDD</option>
-                        <option value="Stage">Stage</option>
-                        <option value="Freelance">Freelance</option>
+                    <select name="statut" required>
+                        <?php
+                            for ($i = 0; $i < count($tabl); $i++) {
+                                echo '<option value="'.$tabl[$i]['Statut'].'">'. 
+                                htmlspecialchars($tabl[$i]['Statut']) .
+                                '</option>';}
+                        ?>
                     </select>
                 </label>
 
                 <label>
                 Catégorie
-                    <select name="type_contrat" required>
-                        <option value="CDI">catégorie</option>
-                        <option value="CDD">CDD</option>
-                        <option value="Stage">Stage</option>
-                        <option value="Freelance">Freelance</option>
+                    <select name="categorie" required>
+                         <?php
+                            for ($i = 0; $i < count($tabl); $i++) {
+                                echo '<option value="'.$tabl[$i]['Categorie'].'">'. 
+                                $tabl[$i]['Categorie'].
+                                '</option>';}
+                         ?>
                     </select>
                 </label>
             </div>
             
 
             <div class="actions">
-            <button type="submit" class="btn">Annuler</button>
-            <button type="reset" class="btn">Enregistrer</button>
+                <button type="submit" name="Enregistrer" class="btn">Enregistrer</button>
+                <button type="reset" name="effacer" class="btn">effacer</button>
             </div>
 
         </form>
     </div>
    </section>
    
-
-<body>
-    <footer class="site-footer">
+<footer class="site-footer">
     <div class="container footer-inner">
         <p>© 2025 LagonJobs — Tous droits réservés</p>
         <b> Confidentialité  <a href="contact.html">Nous contacter</a> </b>
     </div>
 </footer>
-
+</body>
 </html>
 
 
