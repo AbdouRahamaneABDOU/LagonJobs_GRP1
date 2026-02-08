a1b1889c4fc8770195e55c57af7d277decccbeb0
AbdouRahamaneABDOU <ababdou.sio1@gmail.com>
2026-02-08 09:58:16 +0100

    Finalisation de la requête d'ajout d'une offre ainsi que la requête de modification(presque fini)

--- BackOffice/offre.php
+++ BackOffice/offre.php
@@ -1,26 +1,81 @@
 <?php
 require_once(__DIR__ . '/bdd.php');
 
-/*
-if(isset($_POST['metier']) && !empty($_POST['metier']) && isset($_POST['description']) && !empty($_POST['description']) && isset($_POST['mission']) && !empty($_POST['mission']) && isset($_POST['profil']) && !empty($_POST['profil']) && isset($_POST['categorie']) && !empty($_POST['categorie'])){
+//Ajout d'une offre
+if(isset($_POST['metier']) && !empty($_POST['metier']) && 
+isset($_POST['description']) && !empty($_POST['description']) && 
+isset($_POST['mission']) && !empty($_POST['mission']) && 
+isset($_POST['profil']) && !empty($_POST['profil']) && 
+isset($_POST['categorie']) && !empty($_POST['categorie']) && 
+isset($_POST['statut']) && !empty($_POST['statut']) && 
+isset($_POST['contrat']) && !empty($_POST['contrat']) && 
+isset($_POST['modetravail']) && !empty($_POST['modetravail']) && 
+isset($_POST['ville']) && !empty($_POST['ville'])){
   $metier=$_POST['metier'];
   $descrip=$_POST['description'];
   $mission=$_POST['mission'];
   $profil=$_POST['profil'];
   $cate=$_POST['categorie'];
+  $ville=$_POST['ville'];
+  $contract=$_POST['contrat'];
+  $modetravail=$_POST['modetravail'];
+  $statut=$_POST['statut'];
     
-  $sqlQuery = "INSERT INTO job(`Titre`,`Description`,`Missions`,`Profil`,`Categorie`) VALUES (:Titre,:Descrip,:Mission,:Profil,:Cate)";
+  $sqlQuery = "INSERT INTO `offres`(`Titre`, `Description`, `Profil`, `Missions`, `Id_Ville`, `Id_Categorie`, `Id_Contrats`, `Id_ModeTravail`, `Id_Statut`) 
+  VALUES (:Titre,:Descrip,:Profil,:Mission,:IdVille,:IdCate,:IdCont,:IdMode,:IdStat)";
   $insertJob = $mysqlClient->prepare($sqlQuery);
   $insertJob->execute([
     'Titre'=>$metier,
     'Descrip'=>$descrip,
-    'Mission'=>$mission,
     'Profil'=>$profil,
-    'Cate'=>$cate
+    'Mission'=>$mission,
+    'IdVille'=>$ville,
+    'IdCate'=>$cate,
+    'IdCont'=>$contract,
+    'IdMode'=>$modetravail,
+    'IdStat'=>$statut
   ]);
+}
 
-}*/
+//Modification d'une offre
+if(isset($_POST['mod_id']) && 
+isset($_POST['mod_metier']) &&
+isset($_POST['mod_description']) &&
+isset($_POST['mod_mission']) && 
+isset($_POST['mod_profil']) &&
+isset($_POST['mod_categorie']) &&
+isset($_POST['mod_statut']) && 
+isset($_POST['mod_contrat']) &&
+isset($_POST['mod_modetravail']) &&
+isset($_POST['mod_ville'])){
+  $Mod_Id=$_POST['mod_id'];
+  $Modmetier=$_POST['mod_metier'];
+  $Moddescrip=$_POST['mod_description'];
+  $Modmission=$_POST['mod_mission'];
+  $Modprofil=$_POST['mod_profil'];
+  $Modcate=$_POST['mod_categorie'];
+  $Modville=$_POST['mod_ville'];
+  $Modcontract=$_POST['mod_contrat'];
+  $Modmodetravail=$_POST['mod_modetravail'];
+  $Modstatut=$_POST['mod_statut'];
 
+  $sqlQuery = "UPDATE `offres` SET `Titre`=:titre,`Description`=:des,`Profil`=:pro,`Missions`=:mis,`Id_Ville`=:IdV,`Id_Categorie`=:IdCa,`Id_Contrats`=:IdCo,`Id_ModeTravail`=:IdMo,`Id_Statut`=:IdSa WHERE Id=:id";
+  $editOffre = $mysqlClient->prepare($sqlQuery);
+  $editOffre->execute([
+    'id'=> $Mod_Id,
+    'titre'=> $Modmetier,
+    'des'=>$Moddescrip,
+    'pro'=> $Modprofil,
+    'mis'=> $Modmission,
+    'IdV'=> $Modville,
+    'IdCa'=> $Modcate,
+    'IdCo'=> $Modcontract,
+    'IdMo'=> $Modmodetravail,
+    'IdSa'=> $Modstatut
+  ]);
+}
+
+//Jointure  
 $sqlQuery = 
 'SELECT of.Id,
 of.Titre,
@@ -29,15 +84,43 @@ of.Missions,
 of.Profil,
 ca.NomCategorie,
 vi.NomVille,
-st.NomStatut
+st.NomStatut,
+co.TypeContrat,
+mo.NomModeTravail
 FROM offres of 
-JOIN Categorie ca on ca.Id = of.Id_categorie
-JOIN Ville vi on vi.Id = of.Id_ville
-JOIN Statut st on st.Id = of.Id_statut;';
+JOIN categorie ca on ca.Id = of.Id_Categorie
+JOIN ville vi on vi.Id = of.Id_Ville
+JOIN statut st on st.Id = of.Id_Statut
+JOIN contrats co on co.Id = of.Id_Contrats
+JOIN modetravail mo on mo.Id = of.Id_ModeTravail;';
 $SelectOffres=$mysqlClient->prepare($sqlQuery);
 $SelectOffres->execute();
 $Offres=$SelectOffres->fetchAll();
 
+$sqlQuery='SELECT * FROM  categorie';
+$selectCat=$mysqlClient->prepare($sqlQuery);
+$selectCat->execute();
+$Cate=$selectCat->fetchAll();
+
+$sqlQuery='SELECT * FROM  ville';
+$selectVille=$mysqlClient->prepare($sqlQuery);
+$selectVille->execute();
+$Villes=$selectVille->fetchAll();
+
+$sqlQuery='SELECT * FROM  statut';
+$selectStatut=$mysqlClient->prepare($sqlQuery);
+$selectStatut->execute();
+$Statut=$selectStatut->fetchAll();
+
+$sqlQuery='SELECT * FROM  contrats';
+$selectContrat=$mysqlClient->prepare($sqlQuery);
+$selectContrat->execute();
+$Contrats=$selectContrat->fetchAll();
+
+$sqlQuery='SELECT * FROM  modetravail';
+$selectMode=$mysqlClient->prepare($sqlQuery);
+$selectMode->execute();
+$ModeTravail=$selectMode->fetchAll();
 
 ?>
 
@@ -64,12 +147,12 @@ $Offres=$SelectOffres->fetchAll();
         </div>
     </header>
     
-    <section class="hero">
+    <main class="hero">
 
         
         <div class="container">
-            <h1>Ajout un métier</h1>
-            <form action="job.php" method="POST" class="form">
+            <h1>Ajout d'une offre</h1>
+            <form action="offre.php" method="POST" class="form">
 
                 <div>
                     <label>Titre</label>
@@ -83,35 +166,76 @@ $Offres=$SelectOffres->fetchAll();
 
                     <label>Profil</label>
                     <textarea name="profil" required></textarea>
-
+                    
                     <label for="listbox">Ville</label>
                     <select name="ville" >
-                        <option value="1">Dakar</option>
-                        <option value="2">Thies</option>
-                        <option value="3">Saint-Louis</option>
+                        <?php 
+                        for ($i=0;$i<count($Villes);$i++) {
+                            if(isset($_POST['ville']) && $Villes[$i]['Id']===intval($_POST['ville'])) { 
+                                echo "<option value=".$Villes[$i]['Id']."selected>".$Villes[$i]['NomVille']."</option>";
+                            }else{
+                                echo "<option value=".$Villes[$i]['Id'].">".$Villes[$i]['NomVille']."</option>";
+                            }
+                        } ?>
+                    </select>
+
+                    <label for="listbox">Mode de travail</label>
+                    <select name="modetravail" >
+                        <?php 
+                        for ($i=0;$i<count($ModeTravail);$i++) {
+                            if(isset($_POST['modetravail']) && $ModeTravail[$i]['Id']===intval($_POST['modetravail'])) { 
+                                echo "<option value=".$ModeTravail[$i]['Id']."selected>".$ModeTravail[$i]['NomModeTravail']."</option>";
+                            }else{
+                                echo "<option value=".$ModeTravail[$i]['Id'].">".$ModeTravail[$i]['NomModeTravail']."</option>";
+                            }
+                        } ?>
+                    </select>
+
+                    <label for="listbox">Contrat</label>
+                    <select name="contrat" >
+                        <?php 
+                        for ($i=0;$i<count($Contrats);$i++) {
+                            if(isset($_POST['contrat']) && $Contrats[$i]['Id']===intval($_POST['contrat'])) { 
+                                echo "<option value=".$Contrats[$i]['Id']."selected>".$Contrats[$i]['TypeContrat']."</option>";
+                            }else{
+                                echo "<option value=".$Contrats[$i]['Id'].">".$Contrats[$i]['TypeContrat']."</option>";
+                            }
+                        } ?>
                     </select>
 
                     <label for="listbox" name="categorie">Catégorie</label>
                     <select name="categorie" >
-                        <option value="1">Informatique</option>
-                        <option value="2">Marketing</option>
-                        <option value="3">Finance</option>
+                        <?php 
+                        for ($i=0;$i<count($Cate);$i++) {
+                            if(isset($_POST['categorie']) && $Cate[$i]['Id']===intval($_POST['categorie'])) { 
+                                echo "<option value=".$Cate[$i]['Id']."selected>".$Cate[$i]['NomCategorie']."</option>";
+                            }else{
+                                echo "<option value=".$Cate[$i]['Id'].">".$Cate[$i]['NomCategorie']."</option>";
+                            }
+                        } ?>
                     </select>
 
                     <label for="listbox" name="statut">Statut</label>
                     <select name="statut" >
-                        <option value="1">Publiée</option>
-                        <option value="2">Brouillon</option>
+                        <?php 
+                        for ($i=0;$i<count($Statut);$i++) {
+                            if(isset($_POST['statut']) && $Statut[$i]['Id']===intval($_POST['statut'])) { 
+                                echo "<option value=".$Statut[$i]['Id']."selected>".$Statut[$i]['NomStatut']."</option>";
+                            }else{
+                                echo "<option value=".$Statut[$i]['Id'].">".$Statut[$i]['NomStatut']."</option>";
+                            }
+                        } ?>
                     </select>
                     
                     <button type="submit" class="btn">Ajouter</button>
+                    <button type="reset" class="btn">Réinitialiser</button>
                     
                 </div>
             </form>
 
 
         </div>
-
+</br>
 
         <div class="container card">
             <h2>Liste des métiers</h2>
@@ -119,57 +243,79 @@ $Offres=$SelectOffres->fetchAll();
                 <tr>
                     <th>Titre</th>
                     <th>Description</th>
-                    <th>Missions</th>
                     <th>Profil</th>
+                    <th>Missions</th>
                     <th>Catégorie</th>
                     <th>Ville</th>
+                    <th>Contrat</th>
+                    <th>Mode de travail</th>
                     <th>Statut</th>
                     <th>Actions</th>
                 </tr>
-                <?php /*
+                <?php 
 
-                for ($i=0;$i<count($Jobs);$i++) {
+                for ($i=0;$i<count($Offres);$i++) {
                 ?>  
                     <tr>
                         <td>
-                            <?php echo ($i+1); ?>
+                            <?php echo $Offres[$i]['Titre']; ?>
                         </td>
                         <td>
-                            <?php echo $Jobs[$i]['Titre']; ?>
+                            <?php echo $Offres[$i]['Description']; ?>
                         </td>
                         <td>
-                            <?php echo $Jobs[$i]['Description']; ?>
+                            <?php echo $Offres[$i]['Profil']; ?>
                         </td>
                         <td>
-                             <?php echo $Jobs[$i]['Missions']; ?>
+                            <?php echo $Offres[$i]['Missions']; ?>
                         </td>
                         <td>
-                             <?php echo $Jobs[$i]['Profil']; ?>
+                            <?php echo $Offres[$i]['NomCategorie']; ?>
                         </td>
                         <td>
-                             <?php echo $Jobs[$i]['Categorie']; ?>
+                            <?php echo $Offres[$i]['NomVille']; ?>
+                        </td>
+                        <td>
+                            <?php echo $Offres[$i]['TypeContrat']; ?>
+                        </td>
+                        <td>
+                            <?php echo $Offres[$i]['NomModeTravail']; ?>
+                        </td>
+                        <td>
+                            <?php echo $Offres[$i]['NomStatut']; ?>
                         </td>
                          <td>
-                            <button type="submit">Éditer</button >
+                            <form action="offre_edit.php" method="POST">
+                                <input type="hidden" name="id_of_edit" value="<?php echo $Offres[$i]['Id']?>">
+                                <input type="hidden" name="Titre_of_edit" value="<?php echo $Offres[$i]['Titre']?>">
+                                <input type="hidden" name="Desc_of_edit" value="<?php echo $Offres[$i]['Description']?>">
+                                <input type="hidden" name="Profil_of_edit" value="<?php echo $Offres[$i]['Profil']?>">
+                                <input type="hidden" name="Mission_of_edit" value="<?php echo $Offres[$i]['Missions']?>">
+                                <input type="hidden" name="Cat_of_edit" value="<?php echo $Offres[$i]['NomCategorie']?>">
+                                <input type="hidden" name="Ville_of_edit" value="<?php echo $Offres[$i]['NomVille']?>">
+                                <input type="hidden" name="Contract_of_edit" value="<?php echo $Offres[$i]['TypeContrat']?>">
+                                <input type="hidden" name="Mode_of_edit" value="<?php echo $Offres[$i]['NomModeTravail']?>">
+                                <input type="hidden" name="Statut_of_edit" value="<?php echo $Offres[$i]['NomStatut']?>">
+                                <button type="submit">Éditer</button >
+                            </form>
                             <button type="submit">Supprimer</button >
                         </td>
                        
                     </tr>
                 <?php
                 }
-                */?>
+                ?>
             </table>
         </div>
-   </section>
+    </main>
    
-<body>
     <footer class="site-footer">
-    <div class="container footer-inner">
-        <p>© 2025 LagonJobs — Tous droits réservés</p>
-        <b> Confidentialité  <a href="contact.html">Nous contacter</a> </b>
-    </div>
-</footer>
-
+        <div class="container footer-inner">
+            <p>© 2025 LagonJobs — Tous droits réservés</p>
+            <b> Confidentialité  <a href="contacts.php">Nous contacter</a> </b>
+        </div>
+    </footer>
+</body>
 </html>
 
 
--- BackOffice/offre_edit.php
+++ BackOffice/offre_edit.php
@@ -2,11 +2,31 @@
 //modification de l'offre   
 require_once(__DIR__ . '/bdd.php');
 
+$sqlQuery='SELECT * FROM  categorie';
+$selectCat=$mysqlClient->prepare($sqlQuery);
+$selectCat->execute();
+$Cate=$selectCat->fetchAll();
 
-$sqlQuery='SELECT * FROM  job';
-$selectjob=$mysqlClient->prepare($sqlQuery);
-$selectjob->execute();
-$Jobs=$selectjob->fetchAll();
+$sqlQuery='SELECT * FROM  ville';
+$selectVille=$mysqlClient->prepare($sqlQuery);
+$selectVille->execute();
+$Villes=$selectVille->fetchAll();
+
+$sqlQuery='SELECT * FROM  statut';
+$selectStatut=$mysqlClient->prepare($sqlQuery);
+$selectStatut->execute();
+$Statut=$selectStatut->fetchAll();
+
+$sqlQuery='SELECT * FROM  contrats';
+$selectContrat=$mysqlClient->prepare($sqlQuery);
+$selectContrat->execute();
+$Contrats=$selectContrat->fetchAll();
+
+$sqlQuery='SELECT * FROM  modetravail';
+$selectMode=$mysqlClient->prepare($sqlQuery);
+$selectMode->execute();
+$ModeTravail=$selectMode->fetchAll();
+
 ?>
 
 
@@ -36,14 +56,95 @@ $Jobs=$selectjob->fetchAll();
         </div>
     </header>
     
-    <section class="hero">
+    <main class="hero">
 
-    <div class="container">
-        <h1>Modification de l'offre</h1>
+        <div class="container">
+            <h1>Modification de l'offre</h1>
+            <form action="offre.php" method="POST" class="form">
 
-                
-    </div>
-   </section>
+                <div>
+                    <input name="mod_id" type="hidden" value="<?php echo $_POST['id_of_edit']?>">
+
+                    <label>Titre</label>
+                    <input type="text" name="mod_metier" value="<?php echo $_POST['Titre_of_edit']?>">
+                    
+                    <label>description</label>
+                    <textarea name="mod_description" value="<?php echo $_POST['Desc_of_edit']?>"></textarea>
+
+                    <label>Missions</label>
+                    <textarea name="mod_mission" value="<?php echo $_POST['Mission_of_edit']?>"></textarea>
+
+                    <label>Profil</label>
+                    <textarea name="mod_profil" value="<?php echo $_POST['Profil_of_edit']?>"></textarea>
+                    
+                    <label for="listbox">Ville</label>
+                    <select name="mod_ville" >
+                        <?php 
+                        for ($i=0;$i<count($Villes);$i++) {
+                            if(isset($_POST['Ville_of_edit']) && $Villes[$i]['Id']===intval($_POST['Ville_of_edit'])) { 
+                                echo "<option value=".$Villes[$i]['Id']."selected>".$Villes[$i]['NomVille']."</option>";
+                            }else{
+                                echo "<option value=".$Villes[$i]['Id'].">".$Villes[$i]['NomVille']."</option>";
+                            }
+                        } ?>
+                    </select>
+
+                    <label for="listbox">Mode de travail</label>
+                    <select name="mod_modetravail" >
+                        <?php 
+                        for ($i=0;$i<count($ModeTravail);$i++) {
+                            if(isset($_POST['Mode_of_edit']) && $ModeTravail[$i]['Id']===intval($_POST['Mode_of_edit'])) { 
+                                echo "<option value=".$ModeTravail[$i]['Id']."selected>".$ModeTravail[$i]['NomModeTravail']."</option>";
+                            }else{
+                                echo "<option value=".$ModeTravail[$i]['Id'].">".$ModeTravail[$i]['NomModeTravail']."</option>";
+                            }
+                        } ?>
+                    </select>
+
+                    <label for="listbox">Contrat</label>
+                    <select name="mod_contrat" >
+                        <?php 
+                        for ($i=0;$i<count($Contrats);$i++) {
+                            if(isset($_POST['Contract_of_edit']) && $Contrats[$i]['Id']===intval($_POST['Contract_of_edit'])) { 
+                                echo "<option value=".$Contrats[$i]['Id']."selected>".$Contrats[$i]['TypeContrat']."</option>";
+                            }else{
+                                echo "<option value=".$Contrats[$i]['Id'].">".$Contrats[$i]['TypeContrat']."</option>";
+                            }
+                        } ?>
+                    </select>
+
+                    <label for="listbox" name="categorie">Catégorie</label>
+                    <select name="mod_categorie" >
+                        <?php 
+                        for ($i=0;$i<count($Cate);$i++) {
+                            if(isset($_POST['Cat_of_edit']) && $Cate[$i]['Id']===intval($_POST['Cat_of_edit'])) { 
+                                echo "<option value=".$Cate[$i]['Id']."selected>".$Cate[$i]['NomCategorie']."</option>";
+                            }else{
+                                echo "<option value=".$Cate[$i]['Id'].">".$Cate[$i]['NomCategorie']."</option>";
+                            }
+                        } ?>
+                    </select>
+
+                    <label for="listbox" name="statut">Statut</label>
+                    <select name="mod_statut" >
+                        <?php 
+                        for ($i=0;$i<count($Statut);$i++) {
+                            if(isset($_POST['Statut_of_edit']) && $Statut[$i]['Id']===intval($_POST['Statut_of_edit'])) { 
+                                echo "<option value=".$Statut[$i]['Id']."selected>".$Statut[$i]['NomStatut']."</option>";
+                            }else{
+                                echo "<option value=".$Statut[$i]['Id'].">".$Statut[$i]['NomStatut']."</option>";
+                            }
+                        } ?>
+                    </select>
+                    
+                    <button type="submit" class="btn">Modifier</button>
+                    <button type="reset" class="btn">Réinitialiser</button>
+                    
+                </div>
+            </form>
+                    
+        </div>
+</main>
    
 <footer class="site-footer">
     <div class="container footer-inner">
