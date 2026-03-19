dc2bb09a2c97727088937e3009c0dc9614c2803e
Azaly MAHAVITENY <azmaha@sio.sisr>
2026-03-19 06:13:15 +0100

    Problème de connexion réglé.

--- FrontOffice/loginconnexion.php
+++ FrontOffice/loginconnexion.php
@@ -3,9 +3,6 @@ session_start();
 require_once(__DIR__ . '/bdd.php');
 
 
-
-
-
 // Validation du formulaire
 if (isset($_POST['mail']) && isset($_POST['mdp'])) {
     if (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
@@ -14,36 +11,45 @@ if (isset($_POST['mail']) && isset($_POST['mdp'])) {
         // On recupère de l'utilisateur à partir de l'email 
         // SELECT `Email`, `MDP` FROM `user` WHERE 1
 
-        $sqlQuery='SELECT Id,Mail,Password,Id_Role FROM Utilisateurs WHERE Mail = :mail';
+        $sqlQuery='SELECT Nom,Prenom,Mail,Password FROM Utilisateurs WHERE Mail = :mail';
         $selectusers=$mysqlClient->prepare($sqlQuery);
         $selectusers->execute([
         'mail' => $_POST['mail']
         ]);
 
-        $utilisateur=$selectusers->fetchAll();
+        $utilisateur=$selectusers->fetch();
 
-        if (count($utilisateur) <= 0) {
+        if (!$utilisateur) {
             //die('Votre email ou mot de passe est incorrecte. 1');
             die('Aucun utilisateur n existe avec cet email');
         }
 
-        
-        
-        if (password_verify($_POST["mdp"], $utilisateur[0]["Password"])) {
-            echo 'Password is valid!';
+        var_dump($utilisateur);
+
+        echo "<br/>";
+        echo $_POST["mdp"];
+
+        echo "<br/>";
+        echo $utilisateur["Password"];
+        echo "<br/>";
+
+        if (password_verify($_POST["mdp"], $utilisateur["Password"])) {
+
+                $_SESSION['user'] = [
+                'email' => $utilisateur["Mail"],
+                'nom' => $utilisateur["Nom"],
+                'prenom' => $utilisateur["Prenom"],
+            ];
+
+            
+            header('Location: index.php');
+            exit;
+
         } else {
             die('Votre mot de passe est incorrecte.');
         }
 
 
-        $_SESSION['user'] = [
-            'iduser' => $utilisateur[0]["Id"],
-            'mail' => $utilisateur[0]["Mail"],
-            'LeRole' => $utilisateur[0]["Id_role"],
-        ];
-
-        header('Location: index.php');
-        exit;
 
     }
 }
